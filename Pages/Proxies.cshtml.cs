2793d1343646fa8fa9851c7db3d5adda710df05b
REALGREEN\mtuskey <mtuskey@realgreen.com>
2023-02-22 14:18:50 -0500

    testing

---
+++ Pages/Proxies.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using proxygen.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace proxygen.Pages
+{
+    public class ProxiesModel : PageModel
+    {
+        public List<ProxyPageModel> Proxies { get; set; }
+        public PageResult OnGetAsync()
+        {
+            Console.WriteLine("here!");
+
+            var foo = JsonSerializer.Deserialize<List<ProxyPageModel>>(TempData["cards"] as string);
+
+            if(foo== null || foo.Count == 0)
+            {
+                RedirectToPage("Index");
+            }
+
+            Proxies = foo;
+
+            return Page();
+        }
+
+        public void OnPostAsync()
+        {
+
+        }
+    }
+}
